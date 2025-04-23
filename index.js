3f5f41dc3041a10f8d33ad71bd04317c2355f7cd
Minh Quang GM Dev <quangminhgdev204@gmail.com>
2025-04-23 23:13:07 +0700

    1 : Khởi tạo khung sườn dự án và thêm các thư viện cần thiết

---
+++ index.js
@@ -0,0 +1,33 @@
+
+const express = require("express");
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const database = require("./config/database");
+database.connect();
+const routesApi = require("./routes/client/index.route");
+
+const app = express();
+const port = process.env.PORT;
+// CORS
+// Cách 1: Tất cả tên miền được phép truy cập
+app.use(cors());
+
+// Cách 2: Áp dụng cho 1 tên miền cụ thể
+// const corsOptions = {
+//   origin: 'https://abc.com',
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+// End CORS
+
+// parse application/json
+app.use(bodyParser.json());
+
+routesApi(app);
+
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
